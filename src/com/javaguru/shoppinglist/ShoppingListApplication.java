c5fd0cbbf6116165f3a574dfb39a5e894c0c2574
Olesja Vasiljeva <olesjonok@inbox.lv>
2020-01-22 05:14:00 +0200

    Add category, discount and description. Set limits for name (from 3 to 32 included), in discount changed sets from 0% to 100%  and price now have limit (can't be less than 0).

--- src/com/javaguru/shoppinglist/ShoppingListApplication.java
+++ src/com/javaguru/shoppinglist/ShoppingListApplication.java
@@ -19,22 +19,42 @@ class ShoppingListApplication {
                 Integer userInput = Integer.valueOf(scanner.nextLine());
                 switch (userInput) {
                     case 1:
-                        System.out.println("Enter product name: ");
-                        String name = scanner.nextLine();
-                        System.out.println("Enter product price: ");
-                        BigDecimal price = new BigDecimal(scanner.nextLine());
+                        String name;
+                        do {
+                            System.out.println("Enter product name: ");
+                            name = scanner.nextLine();
+                        } while (name.length() < 3 || name.length() > 32);
+                        BigDecimal price;
+                        do {
+                            System.out.println("Enter product price: ");
+                            price = new BigDecimal(scanner.nextLine());
+                        } while (price.signum() == -1);
+                        System.out.println("Enter product category: ");
+                        String category = scanner.nextLine();
+                        int discount;
+                        do {
+                            System.out.println("Enter product discount: ");
+                            discount = Integer.parseInt(scanner.nextLine());
+                        } while (discount > 100 || discount < 0);
+                        System.out.println("Enter product description: ");
+                        String description = scanner.nextLine();
                         Product product = new Product();
                         product.setName(name);
                         product.setPrice(price);
+                        product.setCategory(category);
+                        product.setDiscount(discount);
+                        product.setDescription(description);
                         product.setId(productIdSequence);
                         productRepository.put(productIdSequence, product);
                         productIdSequence++;
                         System.out.println("Result: " + product.getId());
+                        break;
                     case 2:
                         System.out.println("Enter product id: ");
                         long id = scanner.nextLong();
                         Product findProductResult = productRepository.get(id);
                         System.out.println(findProductResult);
+                        break;
                     case 3:
                         return;
                 }
