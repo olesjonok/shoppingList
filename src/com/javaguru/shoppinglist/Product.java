8049ab3c1811ce92382374a6078ca9daa81d6a73
Olesja Vasiljeva <olesjonok@inbox.lv>
2020-01-22 05:05:15 +0200

    Was added category, discount and description.

--- src/com/javaguru/shoppinglist/Product.java
+++ src/com/javaguru/shoppinglist/Product.java
@@ -7,6 +7,9 @@ public class Product {
     private Long id;
     private String name;
     private BigDecimal price;
+    private String category;
+    private int discount;
+    private String description;
 
     public Long getId() {
         return id;
@@ -32,4 +35,27 @@ public class Product {
         this.price = price;
     }
 
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
